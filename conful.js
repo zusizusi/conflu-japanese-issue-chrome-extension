25c0003eaf4f1113958613012d9e47660f85d955
zusizusi <sho.iwasawa.2@tier4.jp>
2024-06-21 21:30:13 +0900

    support for jira

--- conful.js
+++ conful.js
@@ -1,19 +1,18 @@
 previous_cursor_position = 0;
 document.addEventListener('keydown', function(event) {
     var activeField = document.activeElement;
-    
     // role="textbox"の場合は、カーソル位置を取得
     if (activeField.getAttribute('role') === 'textbox') {
         // カーソル位置を取得
         var selection = window.getSelection();
         var cursorPosition = selection.anchorOffset;
 
-        // 入力カーソル位置が 1 以外から 1 に変わった場合、
+        // 入力カーソル位置が 1 以外から 1 に変わった場合
         if ((previous_cursor_position != 1 && previous_cursor_position != 0) && cursorPosition === 1 && event.key === "Process") {
             activeField.focus();
             document.execCommand('insertText', false, ' ');
         }
         previous_cursor_position = cursorPosition;
     }
-});
+}, { capture: true });
 
