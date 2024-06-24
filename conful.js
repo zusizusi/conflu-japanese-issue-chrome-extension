bd1c64e931ba8ac7770c71805bfc063ca1532cd2
zusizusi <sho.iwasawa.2@tier4.jp>
2024-06-24 17:33:06 +0900

    small fix

--- conful.js
+++ conful.js
@@ -1,18 +1,17 @@
-previous_cursor_position = 0;
+previousCursorPosition = 0;
 document.addEventListener('keydown', function(event) {
     var activeField = document.activeElement;
     // role="textbox"の場合は、カーソル位置を取得
     if (activeField.getAttribute('role') === 'textbox') {
-        // カーソル位置を取得
         var selection = window.getSelection();
         var cursorPosition = selection.anchorOffset;
 
         // 入力カーソル位置が 1 以外から 1 に変わった場合
-        if ((previous_cursor_position != 1 && previous_cursor_position != 0) && cursorPosition === 1 &&( event.key === "Process" || event.key=="Shift")) {
+        if ((previousCursorPosition != 1 && previousCursorPosition != 0) && cursorPosition === 1 &&( event.key === "Process" || event.key=="Shift")) {
             activeField.focus();
             document.execCommand('insertText', false, ' ');
         }
-        previous_cursor_position = cursorPosition;
+        previousCursorPosition = cursorPosition;
     }
 }, { capture: true });
 
