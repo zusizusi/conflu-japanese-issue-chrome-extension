61e1878863df6eef5b3d920eff8b0fa5951ddf44
Autumn60 <akiro.harada@tier4.jp>
2024-07-01 15:55:13 +0900

    fix #5 issue
    Signed-off-by: Autumn60 <akiro.harada@tier4.jp>

--- conflu.js
+++ conflu.js
@@ -1,18 +1,22 @@
-let previousCursorPosition = 0;
+let previousCursorPosition = -1;
 const keydownHandler = (event) => {
-    const activeField = document.activeElement;
-    // role="textbox"の場合は、カーソル位置を取得
-    if (activeField.getAttribute('role') === 'textbox') {
-        const selection = window.getSelection();
-        const cursorPosition = selection.anchorOffset;
+  const activeField = document.activeElement;
+  // role="textbox"の場合は、カーソル位置を取得
+  if (activeField.getAttribute("role") === "textbox") {
+    const selection = window.getSelection();
+    const cursorPosition = selection.anchorOffset;
 
-        // 入力カーソル位置が 1 以外から 1 に変わった場合
-        if ((previousCursorPosition !== 1 && previousCursorPosition !== 0) && cursorPosition === 1 && (event.key === "Process" || event.key == "Shift")) {
-            activeField.focus();
-            document.execCommand('insertText', false, ' ');
-        }
-        previousCursorPosition = cursorPosition;
+    // 入力カーソル位置が 1 以外から 1 に変わった場合
+    if (
+      previousCursorPosition !== cursorPosition &&
+      cursorPosition === 1 &&
+      (event.key === "Process" || event.key === "Shift")
+    ) {
+      activeField.focus();
+      document.execCommand("insertText", false, " ");
     }
+    previousCursorPosition = cursorPosition;
+  }
 };
 
-document.addEventListener('keydown', keydownHandler, { capture: true });
+document.addEventListener("keydown", keydownHandler, { capture: true });
