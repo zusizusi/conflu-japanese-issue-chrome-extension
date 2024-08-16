5a9e000ac238d4a78dd2fa2ec0682fe7c3d59225
Sho Iwasawa <41606073+zusizusi@users.noreply.github.com>
2024-08-16 14:16:35 +0300

    fix conversion bug (#9)
    * fix conversion bug
    
    * change version

--- conflu.js
+++ conflu.js
@@ -1,18 +1,10 @@
-let previousCursorPosition = -1;
-const keydownHandler = (event) => {
+const compositionendHandler = (event) => {
     const activeField = document.activeElement;
-    // role="textbox"の場合は、カーソル位置を取得
-    if (activeField.getAttribute('role') === 'textbox') {
-        const selection = window.getSelection();
-        const cursorPosition = selection.anchorOffset;
+    if (activeField && activeField.getAttribute('role') === 'textbox') {
+        activeField.focus();
+        document.execCommand('insertText', false, ' ');
+        document.execCommand('delete', false);
 
-        // 入力カーソル位置が 1 以外から 1 に変わった場合
-        if (previousCursorPosition !== cursorPosition && cursorPosition === 1 && (event.key === "Process" || event.key == "Shift")) {
-            activeField.focus();
-            document.execCommand('insertText', false, ' ');
-        }
-        previousCursorPosition = cursorPosition;
     }
-};
-
-document.addEventListener('keydown', keydownHandler, { capture: true });
+}
+document.addEventListener('compositionend', compositionendHandler, { capture: true });
